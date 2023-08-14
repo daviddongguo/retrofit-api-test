43fb5b0363557e335dc359db0658fe1616311467
Dongguo WU <david.dong.guo@gmail.com>
2023-08-14 12:28:58 -0400

    show chart

--- app/src/main/java/xyz/dongguo/retrofit/MainActivity.java
+++ app/src/main/java/xyz/dongguo/retrofit/MainActivity.java
@@ -9,17 +9,25 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Cartesian;
+import com.anychart.charts.Pie;
+import com.anychart.core.cartesian.series.Line;
+import com.anychart.data.Mapping;
+import com.anychart.data.Set;
+import com.anychart.enums.Orientation;
+import com.anychart.enums.ScaleStackMode;
+import com.anychart.scales.Linear;
+
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-import java.util.concurrent.FutureTask;
 
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
 import io.realm.RealmResults;
-import io.realm.mongodb.App;
-import io.realm.mongodb.AppConfiguration;
-import io.realm.mongodb.sync.SyncConfiguration;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -32,7 +40,9 @@ import xyz.dongguo.retrofit.model.Ingredient;
 public class MainActivity extends AppCompatActivity {
 
     Realm uiThreadRealm;
+
     List<DateTimeCalorie> entries = new ArrayList<DateTimeCalorie>();
+
 
     APIInterface apiInterface;
     TextView responseText;
@@ -46,6 +56,63 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // chart
+        AnyChartView anyChartView = findViewById(R.id.any_chart_view);
+
+        Cartesian cartesian = AnyChart.cartesian();
+
+        cartesian.animation(true);
+
+        cartesian.title("Combination of Stacked Column and Line Chart (Dual Y-Axis)");
+
+        cartesian.yScale().stackMode(ScaleStackMode.VALUE);
+
+        Linear scalesLinear = Linear.instantiate();
+        scalesLinear.minimum(0d);
+        scalesLinear.maximum(100d);
+        scalesLinear.ticks("{ interval: 20 }");
+
+        com.anychart.core.axes.Linear extraYAxis = cartesian.yAxis(1d);
+        extraYAxis.orientation(Orientation.RIGHT)
+                .scale(scalesLinear);
+        extraYAxis.labels()
+                .padding(0d, 0d, 0d, 5d)
+                .format("{%Value}%");
+
+        List<DataEntry> data = new ArrayList<>();
+        data.add(new CustomDataEntry("P1", 96.5, 2040, 1200, 1600));
+        data.add(new CustomDataEntry("P2", 77.1, 1794, 1124, 1724));
+        data.add(new CustomDataEntry("P3", 73.2, 2026, 1006, 1806));
+        data.add(new CustomDataEntry("P4", 61.1, 2341, 921, 1621));
+        data.add(new CustomDataEntry("P5", 70.0, 1800, 1500, 1700));
+        data.add(new CustomDataEntry("P6", 60.7, 1507, 1007, 1907));
+        data.add(new CustomDataEntry("P7", 62.1, 2701, 921, 1821));
+        data.add(new CustomDataEntry("P8", 75.1, 1671, 971, 1671));
+        data.add(new CustomDataEntry("P9", 80.0, 1980, 1080, 1880));
+        data.add(new CustomDataEntry("P10", 54.1, 1041, 1041, 1641));
+        data.add(new CustomDataEntry("P11", 51.3, 813, 1113, 1913));
+        data.add(new CustomDataEntry("P12", 59.1, 691, 1091, 1691));
+
+        Set set = Set.instantiate();
+        set.data(data);
+        Mapping lineData = set.mapAs("{ x: 'x', value: 'value' }");
+        Mapping column1Data = set.mapAs("{ x: 'x', value: 'value2' }");
+        Mapping column2Data = set.mapAs("{ x: 'x', value: 'value3' }");
+        Mapping column3Data = set.mapAs("{ x: 'x', value: 'value4' }");
+
+        cartesian.column(column1Data);
+        cartesian.crosshair(true);
+
+        Line line = cartesian.line(lineData);
+        line.yScale(scalesLinear);
+
+        cartesian.column(column2Data);
+
+        cartesian.column(column3Data);
+
+        anyChartView.setChart(cartesian);
+
+        // end of char
 
         // init Realm
         Realm.init(this); // context, usually an Activity or Application
@@ -77,10 +144,10 @@ public class MainActivity extends AppCompatActivity {
 
                 try {
                     DateTimeCalorie Task = new DateTimeCalorie(1200.0);
-                    uiThreadRealm.executeTransaction (transactionRealm -> {
+                    uiThreadRealm.executeTransaction(transactionRealm -> {
                         transactionRealm.insert(Task);
                     });
-                }catch (Exception ex) {
+                } catch (Exception ex) {
                     Log.d("ERROR", ex.toString());
                 }
 
@@ -103,7 +170,7 @@ public class MainActivity extends AppCompatActivity {
                         Log.d("TAG", response.code() + "");
                         String displayResponse = "";
                         CalorieResults results = response.body();
-                        for(Ingredient ingredient  : results.items){
+                        for (Ingredient ingredient : results.items) {
                             displayResponse += ingredient.name + " :  " + ingredient.calories + "\n";
                         }
 
@@ -119,8 +186,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-    }
+    }// end of onCreate
 
+    private class CustomDataEntry extends ValueDataEntry {
+        CustomDataEntry(String x, Number value, Number value2, Number value3, Number value4) {
+            super(x, value);
+            setValue("value2", value2);
+            setValue("value3", value3);
+            setValue("value4", value4);
+        }
+    } // end of dataentry class
 
 
 }
